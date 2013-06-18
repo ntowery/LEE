fc764d256f9b21f729d0fc86106ca8070aaf03f1
Lee Sagi <lsagi@netflix.com>
2013-06-18 06:34:02 +0000

    .

--- current/current.sql
+++ current/current.sql
@@ -1,6 +1,13 @@
-use default;
+SELECT account_id,
+       CAST(regexp_replace(from_unixtime(unix_timestamp(signup_utc_ts)), ":|-| ", "") AS BIGINT) signup_utc_ts,
+       signup_date,
+       CASE WHEN cancel_date IS NULL THEN 
+                 20200101000000
+            ELSE
+                CAST( CONCAT(CAST(cancel_date AS STRING), '000000') AS BIGINT)
+       END cancel_date
+FROM dse.subscrn_derived_d 
+WHERE account_id = 20675816
+ORDER BY 2 ASC 
+LIMIT 1000000;
 
-SELECT COUNT(*)
-FROM default.ab_nonmember_events
-WHERE dateint >= 20130612 AND
-      other_properties['opType'] = 'ALLOCATE'; 
--- current/kelly_query_3.sql
+++ current/kelly_query_3.sql
@@ -1,19 +1,29 @@
 SELECT a.client_id,
        b.customer_id,
-       a.allocation_date,
-       a.tsp,
+       a.allocation_tsp,
+       c.signup_tsp,
+       c.cancel_tsp,
+       CASE WHEN a.allocation_tsp >= c.signup_tsp AND a.allocation_tsp <= c.cancel_tsp THEN
+                 'y'
+            ELSE 
+                 'n'
+       END is_member_at_allocation,
+       CASE WHEN a.allocation_tsp >= c.cancel_tsp OR a.allocation_tsp >= c.signup_tsp THEN
+                 'y'
+            ELSE
+                 'n'
+       END is_ever_member,
+       CASE WHEN c.signup_tsp > a.allocation_tsp THEN
+                 'y'
+            ELSE 
+                 'n'
+       END is_sgnedup_after_allocation,
        a.cell,
        a.test_id,
-       a.country_code,
-       CASE WHEN c.is_current_member IS NULL THEN
-                 'N'
-            ELSE
-                 c.is_current_member
-       END is_current_member
+       a.country_code
 FROM 
     (SELECT other_properties['clientId'] AS client_id,
-            dateint AS allocation_date,
-            from_unixtime(cast(substr(cast(event_utc_ms AS string), 1, 10) AS bigint), 'yyyyHHddkkmmss') AS tsp,
+            CAST(from_unixtime(cast(substr(cast(event_utc_ms AS string), 1, 10) AS bigint), 'yyyyHHddkkmmss') AS BIGINT) AS allocation_tsp,
             other_properties['cell'] AS cell,
             other_properties['test'] AS test_id,
             other_properties['countryCode'] AS country_code
@@ -36,15 +46,16 @@ LEFT OUTER JOIN
            other_properties['opType'] = 'ASSOCIATE') b 
 ON (a.client_id = b.client_id) 
 LEFT OUTER JOIN
-    (SELECT account_id,
-            snapshot_region_date,
-            'Y' AS is_current_member
-     FROM etl.account_day_d
-     WHERE is_member_active = 1 OR
-           is_on_hold = 1 AND 
-           dateint >= 20130612) c
-ON (b.customer_id = c.account_id AND
-    c.snapshot_region_date = a.allocation_date)
-LIMIT 10;
+   (SELECT account_id,
+           CAST(regexp_replace(from_unixtime(unix_timestamp(signup_utc_ts)), ":|-| ", "") AS BIGINT) signup_tsp,
+           CASE WHEN cancel_date IS NULL THEN
+                     20200101235959
+                ELSE
+                    CAST( CONCAT(CAST(cancel_date AS STRING), '235959') AS BIGINT)
+           END cancel_tsp
+    FROM dse.subscrn_derived_d) c
+ON (b.customer_id = c.account_id)
+WHERE b.customer_id IS NOT NULL
+LIMIT 100;
 
 --client id, account id (if there is one), allocation date, allocation timestamp, test cell, test id, country id or sk, a flag for whether they are a current member (at the point of allocation...not if they sign-up during the test), a flag for whether they are a former member (at the point of allocation), a flag for any other de-allocation that occurred for reasons besides current/former member (and if you don't have this info, this flag is more a nice to have).
